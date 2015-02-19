76d5eb82338416c33657beee70a0e12b8d933322
Vaicel <sarumanlol34@gmail.com>
2015-02-19 18:06:42 +0600

    Update LEDProLibrary.cpp
    Придание библиотеки более красивого вида. Планируются багфиксы

--- LEDProLibrary.cpp
+++ LEDProLibrary.cpp
@@ -1,3 +1,10 @@
+/* 
+Ну собственно говоря, это моя первая библиотека для Arduino. Она создана для того, чтобы упростить вашу работу со светодиодами.
+Пока что есть 4 команды, каждая  представляет из себя стандартные действия со светоиодами, которые вы делали в первые дни знакомства с Arduino.
+Пока еще команды не проверялись на практике, т.к. времени для этого пока не находится.
+LEDProLibrary v 0.0.2a
+*/
+
 #include "LEDProLibrary.h"
 
 void blink(int port,int onTime, int offTime){
@@ -6,59 +13,45 @@ void blink(int port,int onTime, int offTime){
   digitalWrite(port,LOW);
   delay(offTime);
 }
-void lineUp(int startLED, int numberLED, int onTime, int offTime){
-  for(int sLED = startLED; sLED < numberLED+startLED; sLED++){
-	digitalWrite(sLED,HIGH);
-	delay(onTime);
-	digitalWrite(sLED,LOW);
-	delay(offTime);
-  }
-}
-void lineDown(int startLED, int numberLED, int onTime, int offTime){
-  for(int sLED = startLED + numberLED - 1 ; sLED >= startLED; sLED--){
-	digitalWrite(sLED,HIGH);
-	delay(onTime);
-	digitalWrite(sLED,LOW);
-	delay(offTime);
-  }
-}
-void impulseH (int port,int startLight,int step, int stepTime, int multiImp){
-	for(int i = 0; i<= multiImp; i++){
-		for(int st = startLight; st <= 255; st += step){
-			analogWrite(port, st);
-			delay(stepTime);
+void line(int startLED, int numberLED, int onTime, int offTime, bool mode){
+	if(mode = true){
+		for(int sLED = startLED; sLED < numberLED+startLED; sLED++){
+			blink(sLED,onTime,offTime);
+		}
+	}
+	else{
+		for(int sLED = startLED + numberLED - 1 ; sLED >= startLED; sLED--){
+			blink(sLED,onTime,offTime);
 		}
 	}
 }
-void impulseL (int port,int startLight,int step, int stepTime, int multiImp){
-	for(int i = 0; i<= multiImp; i++){
-		for(int st = startLight; st >= 0; st -= step){
-			analogWrite(port, st);
-			delay(stepTime);
+void impulse(int port,int startLight,int step, int stepTime, int multiImp, bool mode){
+	if(mode = true)	{	
+		for(int i = 0; i<= multiImp; i++){
+			for(int st = startLight; st <= 255; st += step){
+				analogWrite(port, st);
+				delay(stepTime);
+			}
+		}	
+	}
+	else{
+		for(int i = 0; i<= multiImp; i++){
+			for(int st = startLight;   st >= 0; st -= step){
+				analogWrite(port, st);
+				delay(stepTime);
+			}
 		}
 	}
 }
-void progressUp (int startLED, int numberLED, int progress){
-	if(progress<numberLED){
-		for(int sLED = startLED; sLED<= startLED + progress;  sLED++){
+void progress(int startLED, int endLED,bool mode){
+	if(mode = true){	
+		for(int sLED = startLED; sLED<=endLED;  sLED++){
 			digitalWrite(sLED, HIGH);
 		}
 	}
 	else{
-		for(int sLED = startLED; sLED<= startLED + numberLED; sLED++){
+		for(int sLED = endLED; sLED>=startLED;  sLED--){
 			digitalWrite(sLED, HIGH);
 		}
+		
 	}
-}
-void progressDown (int startLED, int numberLED, int progress){
-	if(progress<numberLED){
-		for(int sLED = startLED; sLED<= startLED - progress;  sLED--){
-			digitalWrite(sLED, HIGH);
-		}
-	}
-	else{
-		for(int sLED = startLED; sLED<= startLED - numberLED; sLED--){
-			digitalWrite(sLED, HIGH);
-		}
-	}
-}
