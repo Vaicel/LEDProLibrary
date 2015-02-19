90b9c52f50eb274229da0cc488edce7d93d2738d
Vaicel <sarumanlol34@gmail.com>
2015-02-19 18:07:40 +0600

    Update LEDProLibrary.h
    Опять же уменьшение кол-ва функций и  собственно описание данных функций и самой библиотеки.

--- LEDProLibrary.h
+++ LEDProLibrary.h
@@ -1,15 +1,19 @@
+/* 
+Ну собственно говоря, это моя первая библиотека для Arduino. Она создана для того, чтобы упростить вашу работу со светодиодами.
+Пока что есть 4 команды, каждая  представляет из себя стандартные действия со светоиодами, которые вы делали в первые дни знакомства с Arduino.
+Пока еще команды не проверялись на практике, т.к. времени для этого пока не находится.
+LEDProLibrary v 0.0.2a
+*/
+
 #ifndef LEDProLibrary_h
 #define	LEDProLibrary_h
 
 #include "Arduino.h"
 
-void blink(int port,int onTime, int offTime);
-void lineUp(int startLED, int numberLED, int onTime, int offTime);
-void lineDown(int startLED, int numberLED, int onTime, int offTime);
-void impulseH(int port,int startLight,int step, int stepTime, int multiImp);
-void impulseL(int port,int startLight,int step, int stepTime, int multiImp);
-void progressUp(int startLED, int numberLED, int progress);
-void progressDown(int startLED, int numberLED, int progress);
+void blink(int port,int onTime, int offTime); 											//Моргание светодиодом
+void line(int startLED, int numberLED, int onTime, int offTime, bool mode);				//Бегущий огонек
+void impulse(int port,int startLight,int step, int stepTime, int multiImp, bool mode);	//Импульсные световзрывы
+void progress(int startLED, int endLED,bool mode);										//Шкала из светодиодов
 
 	
 #endif
